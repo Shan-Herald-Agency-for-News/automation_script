86a9b07ac69c7060bec2db96aaa23d1ca8d90361
noernova <noernova666@gmail.com>
2022-03-23 20:11:33 +0700

    handle ORGANIZED dir not exist

--- autoganized/moveDirFromOrganized.py
+++ autoganized/moveDirFromOrganized.py
@@ -4,9 +4,11 @@ import shutil
 
 def moveFrom_Organized(parentDir):
     rootDir = os.path.join(parentDir, 'ORGANIZED')
-    for item in os.listdir(rootDir):
-        shutil.move(os.path.join(rootDir, item), os.path.join(parentDir, item))
-    os.rmdir(rootDir)
+    
+    if os.path.isdir(rootDir):
+        for item in os.listdir(rootDir):
+            shutil.move(os.path.join(rootDir, item), os.path.join(parentDir, item))
+        os.rmdir(rootDir)
 
 def walkInDir(cwd):
     for item in os.scandir(cwd):
