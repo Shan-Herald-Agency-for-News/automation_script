a1c442420fa909b8d63184367ac44ed5eecd8381
noernova <noernova666@gmail.com>
2022-07-21 18:09:24 +0700

    create index db script

---
+++ autoganized/listfiles.py
@@ -0,0 +1,51 @@
+import os
+import datetime
+from pathlib import Path
+
+import json
+from bson import json_util
+from collections import OrderedDict
+
+
+def listfiles(parentsDir):
+    id = 0
+    dict_list = []
+
+    for root, dirs, files in os.walk(parentsDir, topdown=True):
+        for file in files:
+            rootPath = os.path.dirname(root)
+            rootPathName = os.path.basename(rootPath)
+
+            fileName, fileType = os.path.splitext(file)
+            filePath = os.path.join(root, file)
+
+            c_time = os.path.getctime(filePath)
+            m_time = os.path.getmtime(filePath)
+
+            dt_c = datetime.datetime.fromtimestamp(c_time)
+            dt_m = datetime.datetime.fromtimestamp(m_time)
+
+            dlist = OrderedDict()
+            dlist["id"] = id
+            dlist["name"] = rootPathName + " - " + fileName
+            dlist["type"] = fileType.split(".")[0]
+            dlist["path"] = filePath
+            dlist["create_time"] = dt_c
+            dlist["modifiled_time"] = dt_m
+            dict_list.append(dlist)
+
+            id = id+1
+    
+    j = json.dumps(dict_list, default=json_util.default, ensure_ascii=False)
+
+    with open("filelist_db.json", "w", encoding="utf8") as f:
+        f.write(j)
+
+def main():
+    cwd = os.getcwd()
+    for item in os.scandir(cwd):
+        parentsDir = Path(item)
+        listfiles(parentsDir)
+
+if __name__ == '__main__':
+    main()
